792bc73679e53023b57331dbadf72a2fb94c239f
Emusanjida <emusanjida4@gmail.com>
2026-01-14 12:49:09 +0600

    Add new product images to wwwroot
    Three new images were added to wwwroot/images for product or content updates. Includes minor updates to Visual Studio and build output files.

--- ElegantBoutiqueHouse/ElegantBoutiqueHouse/obj/Debug/net8.0/ElegantBoutiqueHouse.AssemblyInfo.cs
+++ ElegantBoutiqueHouse/ElegantBoutiqueHouse/obj/Debug/net8.0/ElegantBoutiqueHouse.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElegantBoutiqueHouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e4e1c87d504937bfc2131c19699da2546266327")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53195eccd942a459288e54b71d5784deeb2ba3b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElegantBoutiqueHouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ElegantBoutiqueHouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
